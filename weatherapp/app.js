5a9cfa799a5b940a76d121a42fa6d2d0ffb19a76
Manu Kaushik <deekshabc84@gmail.com>
2025-02-19 21:52:09 +0530

    weatherapp changes

---
+++ weatherapp/app.js
@@ -0,0 +1,24 @@
+let express = require("express");
+let request = require("request");
+let dotenv = require("dotenv")
+dotenv.config()
+let port = process.env.PORT || 7660;
+let app = express();
+
+app.get('/weather',(req,res)=>{
+    let city = req.query.city?req.query.city:'Delhi';
+    // console.log(city);
+    // console.log(req.query);
+    let url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=${process.env.KEY}`
+    //api calling
+    request(url,(err,response)=>{
+        if(err) throw err;
+        const output = JSON.parse(response.body)
+        res.send(output)
+    })
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+        console.log(`Server is running on port ${port}`)
+})
