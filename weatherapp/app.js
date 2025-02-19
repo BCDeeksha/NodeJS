e8d463ffdac1ab0b06d3dc68b4f8751d4462aa36
Manu Kaushik <deekshabc84@gmail.com>
2025-02-19 22:53:28 +0530

    emailnoder

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -1,6 +1,11 @@
-let express = require("express");
-let request = require("request");
-let dotenv = require("dotenv")
+// let express = require("express");
+// let request = require("request");
+// let dotenv = require("dotenv")
+
+import express from 'express';
+import request from 'request';
+import dotenv from 'dotenv';
+
 dotenv.config()
 let port = process.env.PORT || 7660;
 let app = express();
